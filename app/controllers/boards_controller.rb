e8657b5f0a5fb0128bcb61b28eb0c0d3377f593a
takumi555 <htaaa838317@gmail.com>
2021-01-04 17:55:40 +0900

    boardモデルの作成

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,21 @@
 class BoardsController < ApplicationController
   def index
   end
+
+  def new
+
+  end
+
+  def create
+    @board = 
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
